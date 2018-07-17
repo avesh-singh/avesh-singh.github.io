b6656f8cc1acdfcf875f5faf2ab762bf1eaeb9db
avesh-singh <41101418+avesh-singh@users.noreply.github.com>
2018-07-17 15:10:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var myImage = document.querySelector('img');
+var mybutton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+myImage.onclick = function(){
+	var src = myImage.getAttribute('src');
+	if(src === 'images/beach.jpg'){
+		myImage.setAttribute('src','images/daffodil.jpg');
+	}else{
+		myImage.setAttribute('src','images/beach.jpg');
+	}
+}
+
+function setUserName(){
+	var myName = prompt('Please enter your name');
+	localStorage.setItem('name',myName);
+	myHeading.textContent = 'Mozilla is cool, '+ myName;
+	if(!localStorage.getItem('name')){
+		setUserName();
+	}else {
+		var storedName = localStorage.getItem('name');
+		localStorage.textContent = 'Mozilla is cool' + storedName;
+	}
+}
+
+mybutton.onclick = function(){
+	setUserName();
+}
